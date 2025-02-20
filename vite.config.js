15556a7bfac203df3c126e97817a2863c4af8b26
Sanks070 <75072933+Sanks070@users.noreply.github.com>
2025-02-21 01:04:10 +0530

    Update vite.config.js Adding server 2

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    base: './',
-    css: {
-      devSourcemap: false
-    }
-    server: {
-    host: '0.0.0.0', // Allows external access
-    port: 3000,      // Ensure it matches Docker's exposed port
+  plugins: [react()],
+  base: './',
+  css: {
+    devSourcemap: false,
   },
-})
+  server: {
+    host: '0.0.0.0', // Allows external access
+    port: 5173,      // Ensure it matches Docker's exposed port
+    strictPort: true, // Ensure Vite fails if the port is unavailable
+  },
+});
